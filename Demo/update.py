d0db5a1f814b90ac38e7e706c70e1fcf1958e2ba
Sunvick <98928025+Sunvick@users.noreply.github.com>
2022-05-17 23:54:31 +0800

    Update update.py

--- Demo/update.py
+++ Demo/update.py
@@ -4,12 +4,12 @@ try:
     connect = pyodbc.connect(r'Driver= {Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\ichil\Downloads\Database2.accdb')
     print("Connected to a Database")
 
-    Fullname = "Tiffany Kate S. Evangelista"
-    Assignment = 88
-    Laboratory = 93
-    Quiz = 94
-    Exam = 96
-    user_id = 10
+    Fullname = "Sunvick A. Gencianeo"
+    Assignment = 94
+    Laboratory = 91
+    Quiz = 93
+    Exam = 91
+    user_id =10
 
     record = connect.cursor()
     record.execute('UPDATE Table1 SET Fullname = ?, Assignment = ?, Laboratory = ?, Quiz = ?, Exam = ? WHERE id = ?', (Fullname, Assignment, Laboratory, Quiz, Exam, user_id))
